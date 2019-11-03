457ea80e6f829b580b9db60e2ac6066ed402fc70
ramzeto <ramzeto@gmail.com>
2019-11-03 14:48:22 -0600

    When loading the devices, the contents of the bConfigurationValue file is retrieved to detemine if the device is enabled or disabled.

--- usb-toggle.py
+++ usb-toggle.py
@@ -83,7 +83,7 @@ class MainWindow(Gtk.Window):
             if os.path.isdir(entry_file_name):
                 product_file_name = os.path.join(entry_file_name, "product")
                 if os.path.isfile(product_file_name):
-                    self.devices.append(Device(path = product_file_name))
+                    self.devices.append(Device(path = entry_file_name))
 
 
     def show_error(self, error):
@@ -99,13 +99,14 @@ class DeviceGuiController:
 
         self.unlock_check_button = unlock_check_button
         self.unlock_check_button.connect("toggled", self.on_toggled_lock)
+
         self.name_label = name_label
         self.name_label.set_label(self.device.name)
         self.name_label.set_sensitive(False)
 
         self.on_switch = on_switch
         self.on_switch.set_sensitive(False)
-        self.on_switch.set_active(True)
+        self.on_switch.set_active(self.device.on)
         self.on_switch.connect("state-set", self.on_activate)
 
 
@@ -117,8 +118,9 @@ class DeviceGuiController:
 
 
     def on_activate(self, on_switch, state):
+        print(self.device.path)
         regex = re.compile(r'[\w\W]+/([\w\W]*)/product')
-        matches = regex.match(self.device.path)
+        matches = regex.match(os.path.join(self.device.path, "product"))
         if matches:
             device_mount_point = matches.group(1);
             status = 1000
@@ -128,7 +130,7 @@ class DeviceGuiController:
                 status = os.system("echo '" + device_mount_point + "' | tee /sys/bus/usb/drivers/usb/unbind")
 
             if status != 0:
-                self.window.show_error(error = "Failed to change the state of '" + self.device.name + "' (" + self.device.path + ").\nMake sure you have the required permissions.")
+                self.window.show_error(error = "Failed to change the state of '" + self.device.name + "' (" + self.device.path + ").\nMake sure you have the required permissions.\nError " + str(status))
             else:
                 self.window.show_error(error = "")
 
@@ -142,11 +144,21 @@ class Device:
     def __init__(self, path):
         self.path = path
         self.unlocked = False
-        self.on = True
 
-        fp = open(self.path, "r")
+        product_file_name = os.path.join(self.path, "product")
+        fp = open(product_file_name, "r")
         self.name = fp.read().replace("\n", "")
         fp.close()
+
+        configuration_value_file_name = os.path.join(self.path, "bConfigurationValue")
+        fp = open(configuration_value_file_name, "r")
+        configuration_value = fp.read()
+        fp.close()
+
+        if len(configuration_value) > 0 and int(configuration_value) > 0:
+            self.on = True
+        else:
+            self.on = False
 
         
 
